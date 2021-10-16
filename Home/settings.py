e89b28fd1fab3d8eb376c00bd95b338698ccebcb
Kenji <kenji.naj@outlook.com>
2021-10-16 15:42:46 -0500

    conexion a la base

--- Home/settings.py
+++ Home/settings.py
@@ -65,10 +65,14 @@ WSGI_APPLICATION = 'Home.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+    #conexion local
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
