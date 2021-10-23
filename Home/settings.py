483d5edae550595f32e90f1c07a3cd110284863c
Kenji <kenji.naj@outlook.com>
2021-10-23 16:03:49 -0500

    corsheaders

--- Home/settings.py
+++ Home/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     'myapi',
     'rest_framework',
     'django.contrib.admin',
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -68,14 +70,14 @@ from decouple import config
 
 DATABASES = {
     #conexion para heroku
-    #'default': dj_database_url.config(
-    #    default=config('DATABASE_URL')
-    #)
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
     #conexion local
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
@@ -121,6 +123,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
+]
+
+CORS_ORIGIN_WHITELIST=[
+"http://localhost:3000",
+
+]
+
 import django_heroku
 import os
 
