eb57b2978c25503b54feb1aef15ba209719d11b5
KenjiNajera <48227181+KenjiNajera@users.noreply.github.com>
2021-11-03 15:43:18 -0600

    https://crudxempleados.herokuapp.com/

--- Home/settings.py
+++ Home/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:9000",
+    "https://crudxempleados.herokuapp.com/",
 ]
 
 CORS_ORIGIN_WHITELIST=[
